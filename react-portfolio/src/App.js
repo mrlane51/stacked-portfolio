0292c974667c3e7be7eddc58af49161cbce480bb
Lane Lopez <mrlane51@ymail.com>
2022-03-12 22:30:20 -0600

    added css file & component templates

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/header/Header'
+
+function App() {
+    return (
+        <>
+
+        < Header/>
+        {/* < Nav/>
+        < About/>
+        < Experience/>
+        < Services/>
+        < Portfolio/>
+        < Testemonials/>
+        < Contact/>
+        < Footer/> */}
+        
+        </>
+    )
+}
+
+export default App;
