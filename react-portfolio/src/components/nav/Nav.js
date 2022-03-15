e8ffd327eb0ca29dd31113856b416960d3c32efe
Lane Lopez <mrlane51@ymail.com>
2022-03-15 00:02:11 -0500

    added nav bar, with icons from react

---
+++ react-portfolio/src/components/nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+import { GiMushroomHouse, GiPerson, GiEvilBook, GiSecretBook, Gi3DHammer, GiConversation } from 'react-icons/gi';
+
+const Nav = () => {
+    return (
+        <nav>
+        <a href='#'><GiMushroomHouse/></a>
+        </nav>
+    )
+}
+
+export default Nav
