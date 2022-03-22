7dffcfed31148ce21eeffcadda1cf46878be440d
Lane Lopez <mrlane51@ymail.com>
2022-03-21 22:19:34 -0500

    edited routes for router v6 compliance & refactoring components

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
+// import React from 'react';
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Projects from './components/projects/Projects'
+import { Outlet, Link } from 'react-router-dom'
 
-function App() {
+export default function App() {
     return (
-        <>
-        < Header/>
-        < Nav/>
-        < About/>
-    {/* < Experience/>
-        < Services/>
-        < Portfolio/>
-        < Testemonials/>
-        < Contact/>
-        < Footer/> */}
-        
-        </>
+        <div>
+            <h1>Welcome Home</h1>
+                <Link to="/about">About Me</Link>
+                <Link to="/contact">Contact</Link>
+            <Outlet />
+        </div>
     )
 }
-
-export default App;
--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -2,5 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App';
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Projects from './components/projects/Projects'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(
+<BrowserRouter>
+    <Routes>
+        <Route path='/' element={ <App /> } />
+        <Route path='about' element={ <About /> } />
+        <Route path='projects' element={ <Projects /> } />
+        <Route path='contact' element={ <Contact /> } />
+    <Route/>
+    </Routes>
+</BrowserRouter>,
+document.querySelector('#root')
+);
